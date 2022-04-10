fc5c1d2e5c487b42427a3fdd6738dd9f0ee5c5cd
yandutka@mail.ru <yandutka@mail.ru>
2022-04-10 14:00:24 +0300

    Game DONE

--- HomeWork/Form1.Designer.cs
+++ HomeWork/Form1.Designer.cs
@@ -29,10 +29,15 @@ namespace HomeWork
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -40,13 +45,34 @@ namespace HomeWork
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.trackBar1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(592, 58);
+            this.panel1.Size = new System.Drawing.Size(475, 58);
             this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(307, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(307, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
             // 
             // button1
             // 
@@ -87,15 +113,32 @@ namespace HomeWork
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(592, 372);
+            this.dataGridView1.Size = new System.Drawing.Size(475, 372);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button2.Location = new System.Drawing.Point(226, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 58);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Новая игра";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 430);
+            this.ClientSize = new System.Drawing.Size(475, 430);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -115,6 +158,10 @@ namespace HomeWork
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- HomeWork/Form1.cs
+++ HomeWork/Form1.cs
@@ -14,8 +14,10 @@ namespace HomeWork
     {
         //TODO: Создать проект, добавить и настроить в неё dataGridView         //DONE
         //TODO: При создании таблицы, ячейки закрашиваются рандомными цветами   //DONE
-        //TODO: Сделать игру
+        //TODO: Сделать игру //DONE
         int N = 5;
+        int gameStep = 0;
+        int score = 0;
         public Form1()
         {
             InitializeComponent();
@@ -33,6 +35,8 @@ namespace HomeWork
                 dataGridView1.Rows[i].Height = w / n;
                 dataGridView1.Columns[i].Width = w / n;
             }
+            label1.Text = "";
+            label2.Text = "";
         }
         void DyedGrid()
         {
@@ -48,10 +52,9 @@ namespace HomeWork
                         case 2: dataGridView1[i, j].Style.BackColor = Color.Red; break;
                         case 3: dataGridView1[i, j].Style.BackColor = Color.Blue; break;
                     }
-                    //dataGridView1[i, j].Value = null;
+                    dataGridView1[i, j].Value = null;
                 }
             }
-
         }
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
@@ -69,66 +72,76 @@ namespace HomeWork
         private void button1_Click(object sender, EventArgs e)
         {
             DyedGrid();
+            timer1.Enabled = true;
+            label1.Text = $"Количество шагов: {gameStep}";
+            label2.Text = $"Количество очков: {score}";
         }
 
-        void PoofCellGreen(int col, int row)
+        void PoofCell(int col, int row, Color color)
         {
             if (col < 0 || row < 0 || col > dataGridView1.ColumnCount - 1 || row > dataGridView1.RowCount - 1)
                 return;
             if (dataGridView1[col, row].Style.BackColor == Color.White)
                 return;
-            if (dataGridView1[col, row].Style.BackColor == Color.Green)
-            {
+            if (dataGridView1[col, row].Style.BackColor == color)
                 dataGridView1[col, row].Style.BackColor = Color.White;
-            }
             else return;
-            PoofCellGreen(col - 1, row);
-            PoofCellGreen(col, row - 1);
-            PoofCellGreen(col + 1, row);
-            PoofCellGreen(col, row + 1);
+            PoofCell(col - 1, row,color);
+            PoofCell(col, row - 1,color);
+            PoofCell(col + 1, row,color);
+            PoofCell(col, row + 1,color);
         }
-        void PoofCellBlue(int col, int row)
-        {
-            if (col < 0 || row < 0 || col > dataGridView1.ColumnCount - 1 || row > dataGridView1.RowCount - 1)
-                return;
-            if (dataGridView1[col, row].Style.BackColor == Color.White)
-                return;
-            if (dataGridView1[col, row].Style.BackColor == Color.Blue)
-            {
-                dataGridView1[col, row].Style.BackColor = Color.White;
-            }
-            else return;
-            PoofCellBlue(col - 1, row);
-            PoofCellBlue(col, row - 1);
-            PoofCellBlue(col + 1, row);
-            PoofCellBlue(col, row + 1);
-        }
-        void PoofCellRed(int col, int row)
-        {
-            if (col < 0 || row < 0 || col > dataGridView1.ColumnCount - 1 || row > dataGridView1.RowCount - 1)
-                return;
-            if (dataGridView1[col, row].Style.BackColor == Color.White)
-                return;
-            if (dataGridView1[col, row].Style.BackColor == Color.Red)
-            {
-                dataGridView1[col, row].Style.BackColor = Color.White;
-            }
-            else return;
-            PoofCellRed(col - 1, row);
-            PoofCellRed(col, row - 1);
-            PoofCellRed(col + 1, row);
-            PoofCellRed(col, row + 1);
-        }
-
+        
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             int col = e.ColumnIndex;
             int row = e.RowIndex;
-            PoofCellGreen(col, row);
-            PoofCellBlue(col, row);
-            PoofCellRed(col, row);
+            PoofCell(col, row, dataGridView1[col, row].Style.BackColor);
+            label1.Text = $"Количество шагов: {gameStep++ + 1}";
+            label2.Text = $"Количество очков: {score += 20}";
+            if(timer1.Enabled == false)
+            {
+                label1.Text = $"Количество шагов: 0";
+                label2.Text = $"Количество очков: 0";
+            }
             dataGridView1.ClearSelection();
         }
-        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int emptyColumn = 0;
+            for(int col = 0; col < dataGridView1.ColumnCount; col++)
+            {
+                if (dataGridView1[col, dataGridView1.RowCount-1].Style.BackColor == Color.White &&
+                    dataGridView1[col,dataGridView1.RowCount-2].Style.BackColor == Color.White)
+                    emptyColumn++;
+
+                for(int row = dataGridView1.RowCount - 1; row >= 0; row--)
+                {
+                    if(dataGridView1[col,row].Style.BackColor == Color.White)
+                    {
+                        while (row > 0)
+                        {
+                            dataGridView1[col, row].Style.BackColor = dataGridView1[col, row - 1].Style.BackColor;
+                            dataGridView1[col, row - 1].Style.BackColor = Color.White;
+                            row--;
+                        }
+                    }
+                }
+            }
+            if(emptyColumn == dataGridView1.ColumnCount)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show($"Поздравляем, вы победили за {gameStep} шагов, ваши очки {score}");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DyedGrid();
+            label1.Text = $"Количество шагов: {gameStep = 0}";
+            label2.Text = $"Количество очков: {score = 0}";
+            timer1.Enabled = true;
+        }
     }
 }
